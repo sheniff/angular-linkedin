02746878e59bf37e721a4a80e0da45a1d06533f2
Sheniff <sheniff@gmail.com>
2013-12-05 10:53:40 -0800

    Adding LinkedIn OAuth process WIP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,60 +1,111 @@
 'use strict';
 
 angular.module('main', [])
+  config(function(TokenProvider) {
+    var baseUrl = document.URL.replace('example/demo.html', '');
+
+    TokenProvider.extendConfig({
+      clientId: 'tbd',
+      clientSecret: '???',
+      redirectUri: baseUrl + 'src/oauth2callback.html',  // allow lunching demo from a mirror
+      scopes: ["r_fullprofile", "r_emailaddress", "r_network"]
+    });
+  }).
+
   .controller('MainCtrl', ['$scope', 'LinkedIn', 'Distill',
     function ($scope, LinkedIn, Distill) {
 
-      // Proof of Concept: Trying to use 2 different Services with Restangular
-      // ----------------
+      $scope.accessToken = Token.get();
 
-      // ToDo: OAuth process... No idea how yet... Maybe Restangular provides
-      // some way to make it easier? Still to figure out.
+      $scope.authenticate = function() {
+        var extraParams = $scope.askApproval ? {approval_prompt: 'force'} : {};
+
+        // LinkedIn defines a response_type different from the standard 'token'
+        // Done a PR to the project https://github.com/enginous/angular-oauth/pull/12
+        angular.extend(extraParams, {
+          response_type: 'code'
+        });
+
+        var popupOptions = {
+          name: 'LinkedIn Auth'
+        };
+
+        Token.getTokenByPopup(extraParams, popupOptions)
+          .then(function(params) {
+            // Success getting token from popup.
+
+            // Verify the token before setting it, to avoid the confused deputy problem.
+            Token.verifyAsync(params.access_token).
+              then(function(data) {
+                $rootScope.$apply(function() {
+                  $scope.accessToken = params.access_token;
+                  $scope.expiresIn = params.expires_in;
+
+                  Token.set(params.access_token);
+                });
+              }, function() {
+                alert("Failed to verify token.")
+              });
+
+          }, function() {
+            // Failure getting token from popup.
+            alert("Failed to get token from popup.");
+          });
+      };
+
+      $scope.runPoC = function() {
+        // Proof of Concept: Trying to use 2 different Services with Restangular
+        // ----------------
+
+        // ToDo: OAuth process... No idea how yet... Maybe Restangular provides
+        // some way to make it easier? Still to figure out.
 
 
-      // **** Linkedin
-      // (Based on http://developer.linkedin.com/documents/profile-api)
+        // **** Linkedin
+        // (Based on http://developer.linkedin.com/documents/profile-api)
 
-      // To get the current user
-      LinkedIn.people().get('~').then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // To get the current user
+        LinkedIn.people().get('~').then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
 
-      // To get only one person based on id
-      LinkedIn.people().get('id=abcdefg').then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // To get only one person based on id
+        LinkedIn.people().get('id=abcdefg').then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
 
-      // To get only one person based on any other param
-      LinkedIn.people().get('url=http://www.linkedin.com/in/almecija').then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // To get only one person based on any other param
+        LinkedIn.people().get('url=http://www.linkedin.com/in/almecija').then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
 
-      // We could make simplified aliases
-      LinkedIn.me().get().then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // We could make simplified aliases
+        LinkedIn.me().get().then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
 
-      // That allows us to concatenate urls...
-      LinkedIn.me().all('connections').getList().then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // That allows us to concatenate urls...
+        LinkedIn.me().all('connections').getList().then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
 
-      // ...Or making another alias for that
-      LinkedIn.myConnections().getList().then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // ...Or making another alias for that
+        LinkedIn.myConnections().getList().then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
 
-      // **** Distill
-      // (Completely made up)
-      Distill.panels().getList().then(
-        function(data) { console.log('bingo', data); },
-        function(error) { console.log('error', error); }
-      );
+        // **** Distill
+        // (Completely made up)
+        Distill.panels().getList().then(
+          function(data) { console.log('bingo', data); },
+          function(error) { console.log('error', error); }
+        );
+      };
     }
   ]);
---
+++ app/scripts/modules/linkedinOauth.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * A module to include instead of `angularOauth` for a service preconfigured
+ * for LinkedIn OAuth authentication.
+ *
+ * Guide: https://developer.linkedin.com/documents/authentication
+ */
+angular.module('linkedinOauth', ['angularOauth']).
+
+  constant('LinkedInTokenVerifier', function(config, accessToken) {
+    var $injector = angular.injector(['ng']);
+    return $injector.invoke(['$http', '$rootScope', '$q', function($http, $rootScope, $q) {
+      var deferred = $q.defer();
+      var verificationEndpoint = 'https://www.linkedin.com/uas/oauth2/accessToken';
+
+      $rootScope.$apply(function() {
+        $http({method: 'POST', url: verificationEndpoint, params: {
+          grant_type: 'authorization_code',
+          code: accessToken,
+          redirect_uri: config.redirectUri,
+          client_id: config.clientId,
+          client_secret: config.clientSecret
+        }}).
+          success(function(data) {
+            console.log('authorized', data);
+            if (data.audience == config.clientId) {
+              deferred.resolve(data);
+            } else {
+              deferred.reject({name: 'invalid_audience'});
+            }
+          }).
+          error(function(data, status, headers, config) {
+            deferred.reject({
+              name: 'error_response',
+              data: data,
+              status: status,
+              headers: headers,
+              config: config
+            });
+          });
+      });
+
+      return deferred.promise;
+    }]);
+  }).
+
+  config(function(TokenProvider, LinkedInTokenVerifier) {
+    var generateRandomState = function() {
+      // ToDo: To be done properly...
+      return 'XxX';
+    };
+
+    TokenProvider.extendConfig({
+      authorizationEndpoint: 'https://www.linkedin.com/uas/oauth2/authorization',
+      scopes: [],
+      state: generateRandomState(),
+      verifyFunc: LinkedInTokenVerifier
+    });
+  });
+
